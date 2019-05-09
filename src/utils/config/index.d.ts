ab06b05caebc50995de523f6e8465ca8bb0cd2fc
Tang Bo Hao <tech@btang.cn>
2019-05-09 23:25:56 +0800

    * 更新apiEndpoint获取节点地址的方式 * 废弃一系列直接获取coinCfg的方法

--- src/utils/config/index.d.ts
+++ src/utils/config/index.d.ts
@@ -7,80 +7,55 @@ import {
   ChainConfig
 } from "../../models";
 
-interface CoinConfig {
-  name: string
-  chain: string
-  chainKey: string
-  /** 该币种是否启用 */
-  tokenEnabled: boolean
-  /** 该币种充提是否启用 */
-  depositWithdrawEnabled: boolean
-  type: string
-  rate: number
-  basic: TokenConfigBasic
-  jadepool: TokenConfigJadepool
-  /** @deprecated */
-  enabled?: boolean
-  /** @deprecated */
-  disabled?: boolean
-}
-
 /**
  * 根据config.configMods.{type} 将对象进行unset
  * @param types 逗号分隔类型
  * @param jsonObj 处理的对象
  */
 export function applyIgnoreKeys(types: string, jsonObj: object): object
-
-/**
- * 获取CoinCfg,在配置的Coin均可获取
- * @param coinName
- * @param useCached
- */
-export function fetchCoinCfg(coinName: string, useCached?: boolean): CoinConfig
-
 /**
  * 获取区块链配置,仅有效链可获取
  * @param nameOrKeyOrCoreType
  */
 export function fetchChainCfg(nameOrKeyOrCoreType: string): ChainConfig
-
-/**
- * 获取相关链的全部货币名
- * @param chainKey 可选，若不填则返回该进程负责的全部区块链
- */
-export function fetchAllCoinNames(chainKey?: string): string[]
-
 /**
  * 获取该进程负责的全部链名称
  */
 export function fetchAllChainNames(): string[]
-
-/**
- * 获取实时的CoinCfg
- * @param coinName
- * @param useCached
- */
-export function loadCoinCfg(chain: string | ChainConfig, coinName: string): Promise<CoinConfig>
-/**
- * 获取实时的CoinCfg
- * @param coinName 格式需要为 chainKey.coinName
- */
-export function loadCoinCfg(coinName: string): Promise<CoinConfig>
-
 /**
  * 获取实时的区块链配置
  * @param chainKey
  */
 export function loadChainCfg(chainKey: string): Promise<ChainConfig>
-
-/**
- * 获取实时的相关链的全部货币名
- * @param chainKey 可选，若不填则返回该进程负责的全部区块链
- */
-export function loadAllCoinNames(chainKey?: string): Promise<string[]>
-
 /**
  * 获取实时的全部链名称
  */
 export function loadAllChainNames(): Promise<string[]>
+
+
+/**
+ * 该函数已无法使用
+ * @param coinName
+ * @deprecated
+ */
+export function fetchCoinCfg(coinName: string): any
+/**
+ * 该函数已无法使用
+ * @deprecated
+ */
+export function fetchAllCoinNames(chainKey?: string): string[]
+/**
+ * 该函数已无法使用
+ * @deprecated
+ */
+export function loadCoinCfg(chain: string | ChainConfig, coinName: string): Promise<any>
+/**
+ * 该函数已无法使用
+ * @deprecated
+ */
+export function loadCoinCfg(coinName: string): Promise<any>
+/**
+ * 该函数已无法使用
+ * @deprecated
+ */
+export function loadAllCoinNames(chainKey?: string): Promise<string[]>
--- src/utils/config/index.js
+++ src/utils/config/index.js
@@ -2,7 +2,6 @@ const _ = require('lodash')
 const NBError = require('../../NBError')
 const consts = require('../../consts')
 const jp = require('../../jadepool')
-const { mongoose } = require('../db')
 
 const applyIgnoreKeys = (types, jsonObj) => {
   let typeArr = types.split(',')
@@ -12,35 +11,6 @@ const applyIgnoreKeys = (types, jsonObj) => {
     _.forEach(jp.config.configMods[typeKey], key => _.unset(jsonObj, key))
   })
   return jsonObj
-}
-
-const _coinCfgCache = new Map()
-
-const fetchCoinCfg = (coinName, useCached = false) => {
-  const parsedCfg = _.find(jp.config.coins, { name: coinName })
-  if (!parsedCfg) {
-    throw new NBError(20000, `coin: ${coinName}`)
-  }
-  if (!useCached || !_coinCfgCache.has(coinName)) {
-    const basicCfg = _.assign({}, ..._.map(parsedCfg.coinCfgPath, path => _.get(jp.config, path)))
-    const result = {
-      name: coinName,
-      chain: parsedCfg.chain,
-      chainKey: parsedCfg.chainKey,
-      type: basicCfg.Type,
-      rate: basicCfg.Rate,
-      tokenEnabled: true,
-      depositWithdrawEnabled: !basicCfg.disabled,
-      basic: basicCfg,
-      jadepool: _.assign({}, ..._.map(parsedCfg.jpCfgPath, path => _.get(jp.config, path))),
-      /** @deprecated */
-      disabled: basicCfg.disabled,
-      /** @deprecated */
-      enabled: !basicCfg.disabled
-    }
-    _coinCfgCache.set(coinName, result)
-  }
-  return _coinCfgCache.get(coinName)
 }
 
 const fetchChainCfg = (nameOrKeyOrCoreType) => {
@@ -54,65 +24,11 @@ const fetchChainCfg = (nameOrKeyOrCoreType) => {
   return chainCfg
 }
 
-const fetchAllCoinNames = (chainKey) => {
-  const allChains = !chainKey ? fetchAllChainNames() : [ chainKey ]
-  return _.reduce(allChains, (allCoins, chainKey) => {
-    return _.concat(allCoins, _.map(_.filter(jp.config.coins, { Chain: fetchChainCfg(chainKey).Chain }), 'name'))
-  }, [])
-}
-
 const fetchAllChainNames = () => {
   if (jp.env.server === consts.SERVER_TYPES.MAIN && jp.env.processType === consts.PROCESS.TYPES.BLOCKCHAIN) {
     return jp.env.param ? [ jp.env.param ] : []
   } else {
     return _.keys(jp.config.chain)
-  }
-}
-
-/**
- * 获取实时的币种配置
- * @param {string|object} chain
- * @param {string} coinName
- */
-const loadCoinCfg = async (chain, coinName) => {
-  if (coinName === undefined && typeof chain === 'string') {
-    [ chain, coinName ] = chain.split('.')
-  }
-  const chainCfg = await loadChainCfg(chain)
-  if (!chainCfg || !chainCfg.tokens) {
-    throw new NBError(10001, `failed to load chain: ${chain}/${coinName}`)
-  }
-  // Config in DB
-  const ConfigDat = jp.getModel(consts.MODEL_NAMES.CONFIG_DATA)
-  const queryBase = { path: 'tokens', parent: mongoose.Types.ObjectId(chainCfg.id) }
-  const coinDat = await ConfigDat.findOne(Object.assign({ key: coinName }, queryBase)).exec()
-  if (!coinDat) {
-    throw new NBError(20000, `coin: ${coinName}`)
-  }
-  const coinCfg = coinDat.toMerged()
-  // 判断是否加载基础类
-  if (chainCfg.tokens.build.extends) {
-    const baseCoinDat = await ConfigDat.findOne(Object.assign({ key: chainCfg.tokens.build.base }, queryBase)).exec()
-    if (baseCoinDat) {
-      const baseCoinCfg = baseCoinDat.toMerged()
-      coinCfg.coin = _.merge({}, baseCoinCfg.coin, coinCfg.coin)
-      coinCfg.jadepool = _.merge({}, baseCoinCfg.jadepool, coinCfg.jadepool)
-    }
-  }
-  // 构造 result
-  return {
-    name: coinName,
-    chain: chainCfg.Chain,
-    chainKey: chainCfg.key,
-    // 状态标记
-    tokenEnabled: chainCfg.tokens.enabled.indexOf(coinName) !== -1,
-    depositWithdrawEnabled: !coinCfg.coin.disabled,
-    // Coin数据
-    basic: coinCfg.coin,
-    type: coinCfg.coin.Type,
-    rate: coinCfg.coin.Rate,
-    // Jadepool数据
-    jadepool: coinCfg.jadepool
   }
 }
 
@@ -124,41 +40,17 @@ const loadChainCfg = async (chainKey) => {
   if (typeof chainKey !== 'string') return chainKey
   // Config in DB
   const ConfigDat = jp.getModel(consts.MODEL_NAMES.CONFIG_DATA)
-  const query = { path: 'chain', key: chainKey }
-  const chainDat = await ConfigDat.findOne(query).exec()
+  const chainDat = await ConfigDat.findOne({
+    path: 'chain',
+    key: chainKey,
+    parent: { $exists: false }
+  }).exec()
   if (!chainDat) return null
   const chainCfg = chainDat.toMerged()
   // 设置额外参数
   chainCfg.id = String(chainDat._id)
   chainCfg.key = chainKey
-  // 设置tokens配置
-  const tokensDat = await ConfigDat.findOne({ path: 'tokens', key: '', parent: chainDat }).exec()
-  if (tokensDat) {
-    chainCfg.tokens = tokensDat.toMerged()
-  }
   return chainCfg
-}
-
-/**
- * 获取实时的全部已启用的币种名称
- * @param {string} [chainKey=undefined]
- */
-const loadAllCoinNames = async (chainKey) => {
-  // Config in DB
-  const ConfigDat = jp.getModel(consts.MODEL_NAMES.CONFIG_DATA)
-  const query = {
-    path: 'chain',
-    key: typeof chainKey === 'string' ? chainKey : { $ne: '' }
-  }
-  const cfgs = (await ConfigDat.find(query).exec()) || []
-  const enabledChains = cfgs.filter(cfg => !cfg.disabled)
-  const coinNamesArr = await Promise.all(enabledChains.map(async (chainDat) => {
-    const tokensDat = await ConfigDat.findOne({ path: 'tokens', key: '', parent: chainDat }).exec()
-    if (!tokensDat) return []
-    const tokensCfg = tokensDat.toMerged()
-    return (tokensCfg.enabled || []).map(name => `${chainDat.key}.${name}`)
-  }))
-  return _.flatten(coinNamesArr)
 }
 
 /**
@@ -173,15 +65,22 @@ const loadAllChainNames = async () => {
   return cfgs.filter(cfg => !cfg.disabled).map(cfg => cfg.key)
 }
 
+// ------------------------ 已废弃方法 ------------------------
+const fetchCoinCfg = () => { throw new NBError(10001, `unsupported method[fetchCoinCfg]`) }
+const fetchAllCoinNames = () => { throw new NBError(10001, `unsupported method[fetchAllCoinNames]`) }
+const loadCoinCfg = () => { throw new NBError(10001, `unsupported method[loadCoinCfg]`) }
+const loadAllCoinNames = () => { throw new NBError(10001, `unsupported method[loadAllCoinNames]`) }
+
 module.exports = {
   applyIgnoreKeys,
-  fetchCoinCfg,
   fetchChainCfg,
-  fetchAllCoinNames,
   fetchAllChainNames,
   // 实时读取
-  loadCoinCfg,
   loadChainCfg,
-  loadAllCoinNames,
-  loadAllChainNames
+  loadAllChainNames,
+  // 废弃方法
+  fetchCoinCfg,
+  fetchAllCoinNames,
+  loadCoinCfg,
+  loadAllCoinNames
 }
