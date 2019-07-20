48d5bb6f03ffdc5f1c2566d282c0f486a5ff053e
Tang Bo Hao <tech@btang.cn>
2019-07-20 16:48:39 +0800

    调整config.service实现

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -47,47 +47,42 @@ class RedisConfigService extends ConfigService {
   }
   // 便捷查询方法
   async loadChainCfg (chainKey) {
-    const redisKey = REDIS_CFG_CACHE_PREFIX + `CHAINS:${chainKey}:MAIN`
-    const getAsync = promisify(this.redisClient.GET).bind(this.redisClient)
-
-    const resultStr = await getAsync(redisKey)
-    if (!resultStr) return null
-    try {
-      return JSON.parse(resultStr)
-    } catch (err) {
-      return null
-    }
+    const chainCfg = await this.loadConfig('chain', chainKey)
+    chainCfg.key = chainKey
+    return chainCfg
   }
   async loadCoinCfg (chainKey, tokenNameOrAssetIdOrContract) {
-    const aliasKey = REDIS_CFG_CACHE_PREFIX + `CHAINS:${chainKey}:ALIAS`
+    const baseKey = REDIS_CFG_CACHE_PREFIX + `CHAINS:${chainKey}:`
     const hgetAsync = promisify(this.redisClient.HGET).bind(this.redisClient)
     const getAsync = promisify(this.redisClient.GET).bind(this.redisClient)
 
-    let coinName = await hgetAsync(aliasKey, tokenNameOrAssetIdOrContract)
+    let coinName = await hgetAsync(baseKey + 'ALIAS', tokenNameOrAssetIdOrContract)
     coinName = coinName || tokenNameOrAssetIdOrContract
-
-    const redisKey = REDIS_CFG_CACHE_PREFIX + `CHAINS:${chainKey}:TOKENS:${coinName}`
-    const resultStr = await getAsync(redisKey)
-    if (!resultStr) return null
-    try {
-      return JSON.parse(resultStr)
-    } catch (err) {
-      return null
+    
+    let parentId = await getAsync(baseKey + 'ID')
+    if (!parentId) {
+      const chainCfg = await this.loadConfig('chain', chainKey)
+      if (chainCfg) parentId = chainCfg.id
     }
+    if (!parentId) return null
+    const tokenCfg = await this.loadConfig('tokens', coinName, parentId)
+    tokenCfg.name = coinName
+    return tokenCfg
   }
   async loadAllChainNames (includeDisabled = false) {
-    const redisKeys = [ REDIS_CFG_CACHE_PREFIX + `CHAINS_ENABLED` ]
-    if (includeDisabled) redisKeys.push(REDIS_CFG_CACHE_PREFIX + `CHAINS_DISABLED`)
-    const sunionAsync = promisify(this.redisClient.SUNION).bind(this.redisClient)
-    return sunionAsync(redisKeys)
+    return this.loadConfigKeys('chain', undefined, includeDisabled)
   }
   async loadAllCoinNames (chainKey, includeDisabled = false) {
-    const baseKey = REDIS_CFG_CACHE_PREFIX + `CHAINS:${chainKey}:`
-    const redisKeys = [ baseKey + 'ENABLED' ]
-    if (includeDisabled) redisKeys.push(baseKey + 'DISABLED')
+    const baseKey = REDIS_CFG_CACHE_PREFIX + `CHAINS:${chainKey}:ID`
+    const getAsync = promisify(this.redisClient.GET).bind(this.redisClient)
 
-    const sunionAsync = promisify(this.redisClient.SUNION).bind(this.redisClient)
-    return sunionAsync(redisKeys)
+    let parentId = await getAsync(baseKey)
+    if (!parentId) {
+      const chainCfg = await this.loadConfig('chain', chainKey)
+      if (chainCfg) parentId = chainCfg.id
+    }
+    if (!parentId) return null
+    return this.loadConfigKeys('tokens', parentId, includeDisabled)
   }
   // 通用方法
   setAutoSaveWhenLoad (flag) {
@@ -96,17 +91,33 @@ class RedisConfigService extends ConfigService {
   setAliasConfigPath (cfgPath, key, aliasPath) {
     throw new NBError(20205, `invalid method: setAliasConfigPath`)
   }
-  async loadConfigKeys (path, parent = undefined) {
+  async loadConfigKeys (path, parent = undefined, includeDisabled = true) {
+    const pathId = path + `@${parent || 'root'}`
+    const baseKey = REDIS_CFG_CACHE_PREFIX + `KEYS:${pathId}:`
+    const redisKeys = [ baseKey + 'ENABLED' ]
+    if (includeDisabled) redisKeys.push(baseKey + 'DISABLED')
 
+    const sunionAsync = promisify(this.redisClient.SUNION).bind(this.redisClient)
+    return sunionAsync(redisKeys)
   }
-  async loadGeneralCfg (path, key, parent = undefined) {
+  async loadConfig (path, key, parent = undefined) {
+    const pathId = path + `@${parent || 'root'}`
+    const redisKey = REDIS_CFG_CACHE_PREFIX + `DATA:${pathId}:${key}`
+    const getAsync = promisify(this.redisClient.GET).bind(this.redisClient)
 
+    const resultStr = await getAsync(redisKey)
+    if (!resultStr) return null
+    try {
+      return JSON.parse(resultStr)
+    } catch (err) {
+      return null
+    }
   }
-  async saveGeneralCfg (path, key, modJson, disabled = false, parent = undefined) {
-    throw new NBError(20205, `invalid method: saveGeneralCfg`)
+  async saveConfig (path, key, modJson, disabled = false, parent = undefined) {
+    throw new NBError(20205, `invalid method: saveConfig`)
   }
-  async deleteGeneralCfg (path, key, parent = undefined) {
-    throw new NBError(20205, `invalid method: deleteGeneralCfg`)
+  async deleteConfig (path, key, parent = undefined) {
+    throw new NBError(20205, `invalid method: deleteConfig`)
   }
 }
 
@@ -127,16 +138,59 @@ class HostConfigService extends RedisConfigService {
       const port = parseInt(await incrAsync(REDIS_HOST_COUNTER_KEY)) + DEFAULT_PORT
       rpcServer = await jadepool.registerService(consts.SERVICE_NAMES.JSONRPC_SERVER, { host, port })
     }
-    // 添加自定义方法
-    rpcServer.addAcceptableMethod('')
+    
+    // 代理函数
+    const allMethodDescs = Object.getOwnPropertyDescriptors(HostConfigService.prototype)
+    for (const methodKey in allMethodDescs) {
+      if (methodKey === 'constructor' || methodKey === 'initialize' || methodKey === 'onDestroy') continue
+      rpcServer.addAcceptableMethod(methodKey, async (params, ws) => this[methodKey].apply(this, _.values(params)))
+    }
   }
   /**
    * 该Service的优雅退出函数
    * @param signal 退出信号
    */
   async onDestroy (signal) {
-    // TODO 退出rpcServer注册
-    return super.onDestroy(signal)
+    // 退出rpcServer注册
+    await super.onDestroy(signal)
+    let rpcServer = jadepool.getService(consts.SERVICE_NAMES.JSONRPC_SERVER)
+    // 移除方法
+    const allMethodDescs = Object.getOwnPropertyDescriptors(HostConfigService.prototype)
+    for (const methodKey in allMethodDescs) {
+      if (methodKey === 'constructor' || methodKey === 'initialize' || methodKey === 'onDestroy') continue
+      rpcServer.removeAcceptableMethod(methodKey)
+    }
+  }
+  // 便捷查询方法
+  async loadChainCfg (chainKey) {
+    let chainCfg = await super.loadChainCfg(chainKey)
+    if (chainCfg) return chainCfg
+    // 本地读取
+  }
+  async loadCoinCfg (chainKey, tokenNameOrAssetIdOrContract) {
+    // TODO
+  }
+  async loadAllChainNames (includeDisabled = false) {
+    // TODO
+  }
+  // 通用方法
+  setAutoSaveWhenLoad (flag) {
+    // TODO
+  }
+  setAliasConfigPath (cfgPath, key, aliasPath) {
+    // TODO
+  }
+  async loadConfigKeys (path, parent = undefined) {
+    // TODO
+  }
+  async loadConfig (path, key, parent = undefined) {
+    // TODO
+  }
+  async saveConfig (path, key, modJson, disabled = false, parent = undefined) {
+    // TODO
+  }
+  async deleteConfig (path, key, parent = undefined) {
+    // TODO
   }
 }
 
