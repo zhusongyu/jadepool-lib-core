aad056f3c87f0d38ef9f100cbf83b2166208d38c
Tang Bo Hao <tech@btang.cn>
2019-09-02 12:28:57 +0800

    添加pm2service

--- src/services/pm2.service.js
+++ src/services/pm2.service.js
@@ -1,6 +1,8 @@
+const _ = require('lodash')
 const pm2 = require('pm2')
 const { promisify } = require('util')
 const BaseService = require('./core')
+const NBError = require('../NBError')
 const consts = require('../consts')
 const jadepool = require('../jadepool')
 
@@ -73,31 +75,159 @@ class Service extends BaseService {
    * @param {'app'|'task'|'agent'} [opts.mode='task'] 进程模式
    * @param {String} opts.param 进程参数
    * @param {String} [opts.task=undefined] 进程任务名称
-   * @param {String} [opts.jobs=undefined] 进程负责的多个任务名称
+   * @param {String|String[]} [opts.jobs=undefined] 进程负责的多个任务名称
+   * @param {Number} [opts.timeout=undefined] 退出进程的超时
+   * @param {String} [opts.cwd=undefined] 启动运行路径
+   * @param {String} [opts.script=undefined] 启动脚本
+   * @param {Boolean} [opts.cluster=false] 是否启动cluster模式
+   * @param {Number} [opts.instances=1] cluster模式下启动的进程数
    */
   async start (opts) {
+    const mode = opts.mode || 'task'
+    const supportedModes = ['app', 'task', 'agent']
+    if (supportedModes.indexOf(mode) === -1) {
+      throw new NBError(10002, `unsupported mode`)
+    }
 
+    // 设置worker名字
+    let workerProcessName = this.processPrefix
+    if (opts.param) {
+      workerProcessName += `-${opts.param}`
+    }
+    if (opts.task) {
+      workerProcessName += `-${opts.task}`
+    }
+    // 操作进程启动
+    const workerEnv = {
+      JP_MODE: mode,
+      JP_PARAM: opts.param || '',
+      JP_TASK: opts.task || '',
+      NODE_ENV: jadepool.env.name
+    }
+    if (opts.jobs) {
+      workerEnv.JP_JOBS = _.isArray(opts.jobs) ? opts.jobs.join(',') : opts.jobs
+    }
+    // 添加外部workerEnv
+    for (const key in jadepool.env) {
+      const workerEnvPrefix = 'workerEnv'
+      if (!key.startsWith(workerEnvPrefix)) continue
+      const newKey = _.snakeCase('jp' + key.substr(workerEnvPrefix.length)).toUpperCase()
+      workerEnv[newKey] = jadepool.env[key]
+    }
+    const startOpts = {
+      name: workerProcessName,
+      script: opts.script || jadepool.env.script,
+      cwd: opts.cwd || process.cwd(),
+      kill_timeout: opts.timeout || 1000 * 60 * 3, // timeout为3分钟
+      force: true, // 强制启动新进程
+      watch: false,
+      env: workerEnv
+    }
+    if (opts.cluster) {
+      startOpts.exec_mode = 'cluster'
+      startOpts.instances = opts.instances || 1
+    }
+    // 启动进程
+    const re = await pm2Start(startOpts)
+    let result
+    if (re && re[0]) {
+      result = Object.assign(this.parsePM2Result(re[0]), { env: workerEnv })
+    }
+    if (!result) {
+      throw new NBError(21003, `worker.name=${workerProcessName}`)
+    }
+    return result
   }
 
   /**
    * 重启程序
+   * @param {String|Number} nameOrId
    */
-  async restart (processName) {
-
+  async restart (nameOrId) {
+    let result
+    // 由PM2管理进程
+    let re = await pm2Restart(nameOrId)
+    if (re && re[0]) {
+      result = this.parsePM2Result(re[0])
+    } else {
+      throw new NBError(21003, `nameOrId=${nameOrId}`)
+    }
+    return result
   }
 
   /**
    * 关闭程序
+   * @param {String|Number} nameOrId
    */
-  async stop (processName, isDelete = false) {
+  async stop (nameOrId, isDelete = false) {
+    // 对已有进程的启动，无需关注其他信息
+    let result
+    let re = isDelete ? (await pm2Delete(nameOrId)) : (await pm2Stop(nameOrId))
+    if (re && re[0]) {
+      result = this.parsePM2Result(re[0])
+    } else {
+      throw new NBError(21003, `nameOrId=${nameOrId}`)
+    }
+    return result
+  }
 
+  /**
+   * 查询信息
+   * @param {String|Number} nameOrId
+   */
+  async info (nameOrId) {
+    let list = await this.list()
+    return list.find(t => {
+      return t.name === nameOrId || `${t.worker_id}` === `${nameOrId}`
+    })
   }
 
   /**
    * 返回全部相关进程
    */
   async list () {
+    // 由PM2管理进程
+    const list = await pm2List()
+    return list.filter(o => o.name.startsWith(this.processPrefix)).map(t => this.parseItem)
+  }
 
+  /**
+   * 返回简单结果
+   * @param {Object} t
+   */
+  parsePM2Result (t) {
+    return {
+      name: t.name,
+      worker_id: t.pm_id,
+      pid: undefined,
+      status: t.status,
+      restarts: t.restart_time,
+      unstable_restarts: t.unstable_restarts
+    }
+  }
+  /**
+   * 返回结果
+   * @param {Object} t
+   */
+  parseProcessStatus (t) {
+    return {
+      name: t.name,
+      worker_id: t.pm_id,
+      uuid: t.pm2_env.unique_id,
+      pid: t.pid,
+      monit: t.monit,
+      status: t.pm2_env.status,
+      uptime: t.pm2_env.pm_uptime,
+      restarts: t.pm2_env.restart_time,
+      unstable_restarts: t.pm2_env.unstable_restarts,
+      env: {
+        JP_MODE: t.pm2_env.JP_MODE,
+        JP_PARAM: t.pm2_env.JP_PARAM || undefined,
+        JP_TASK: t.pm2_env.JP_TASK || undefined,
+        JP_JOBS: t.pm2_env.JP_JOBS || undefined,
+        NODE_ENV: t.pm2_env.NODE_ENV
+      }
+    }
   }
 }
 
